95106d5abdbb3084b39504608d644a08cf8b4f8f
Volodymyr Tesliuk <volodymyr.tesliuk.v8@gmail.com>
2023-05-15 12:10:00 +0300

    Updated build files

--- build/src/main.js
+++ build/src/main.js
@@ -1,11 +1,36 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY;
-console.log(SECRET_JWT_KEY);
 const initialData = {
     'voltes@gmail.com': {
         id: 123,
@@ -19,19 +44,19 @@ input UserLoginInput {
   email: String
   password: String
 }
-type AccessTokenData {
+type AccessToken {
     token: String
     tokenType: String
 }
-type UserData {
+type User {
     id: ID
 }
-type LoginUserData {
-    user: UserData
-    accessToken: AccessTokenData
+type LoginUser {
+    user: User
+    accessToken: AccessToken
 }
 type Mutation {
-    loginUser(input: UserLoginInput): LoginUserData
+    loginUser(input: UserLoginInput): LoginUser
 }
 type Query {
     sayHi: String
@@ -61,15 +86,12 @@ const rootValue = {
     },
     sayHi: () => 'Hello there!'
 };
-// const app = express()
-// app.use(cors())
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema,
-//     rootValue,
-//     graphiql: true
-//   })
-// )
-// app.listen(4000)
-// console.log('Running a GraphQL API server at localhost:4000/graphql')
+const app = express();
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
